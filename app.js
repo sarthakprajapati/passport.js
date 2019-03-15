6850ce197ca58ea0ad74a423e8eeae3181f5c92a
Sarthak Prajapati <sarthakprajapati@live.in>
2019-03-15 18:22:58 +0530

    login,logout page

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const appRoutes = require('./routes/auth-routes');
+
 const app = express();
 
 // set up view engine
 app.set('view engine','ejs');
+
+app.use('/auth',appRoutes);
 
 // create home route
 app.get('/',(req,res)=>{
